ea8460caa8dc6f3f7f4a812dcac6e755d3212c6c
mandilinkybl-pixel <mandilink.ybl@gmail.com>
2025-09-06 03:19:43 +0530

    Initial commit

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(
 
 app.use(flash());
 
-// Make flash messages available in all views
+  // Make flash messages available in all views
 app.use((req, res, next) => {
   res.locals.success_msg = req.flash("success_msg");
   res.locals.error_msg = req.flash("error_msg");
