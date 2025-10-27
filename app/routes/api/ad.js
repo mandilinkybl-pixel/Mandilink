05330215693b4de1dcef3a84dbd2bf11f5973b01
mandilinkybl-pixel <mandilink.ybl@gmail.com>
2025-10-27 23:26:31 +0530

    Update ad.js

--- app/routes/api/ad.js
+++ app/routes/api/ad.js
@@ -5,6 +5,18 @@ const adController = require("../../controller/api/aj");
 // Get all ads
 router.get("/", adController.getAllAds);
 
+// Get Top Ads
+router.get("/top", adController.getTopAds);
+
+// Get Bottom Ads
+router.get("/bottom", adController.getBottomAds);
+
+// Get Side Ads
+router.get("/side", adController.getSideAds);
+
+// Get Sponsored Ads
+router.get("/sponsored", adController.getSponsoredAds);
+
 // Get ad by ID
 router.get("/:id", adController.getAdById);
 
